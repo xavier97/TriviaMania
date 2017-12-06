22b6ea83bdb1e3fb64058982b31e2bacfa952bef
Xavier <xad2@pitt.edu>
2017-12-06 10:32:56 -0500

    New view controller and deleted uneeded file

---
+++ TriviaMania/TriviaMania/TriviaViewController1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace MobileAppClass
+{
+    public partial class TriviaViewController1 : UIViewController
+    {
+        public TriviaViewController1() : base("TriviaViewController1", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
