a727679496c3a3cd1c633ef650d35d03841f65fc
Xavier <xad2@pitt.edu>
2017-12-08 10:30:59 -0500

    Object details

--- TriviaMania/TriviaMania/TriviaQuestionsRecord.cs
+++ TriviaMania/TriviaMania/TriviaQuestionsRecord.cs
@@ -3,8 +3,15 @@ namespace MobileAppClass
 {
     public class TriviaQuestionsRecord
     {
+        string question { get; set; }
+        string answer { get; set; }
+        string falseQ1 { get; set; }
+        string falseQ2 { get; set; }
+        string falseQ3 { get; set; }
+
         public TriviaQuestionsRecord()
         {
         }
+
     }
 }
