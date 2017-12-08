fef9ae6327de57e7a8e70c4b2764b11a4603b11e
Nick <pacificonm@gmail.com>
2017-12-08 10:31:51 -0500

    Tableview Changes

--- TriviaMania/TriviaMania/QuestionsViewController.designer.cs
+++ TriviaMania/TriviaMania/QuestionsViewController.designer.cs
@@ -1,6 +1,6 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
+// This file has been generated automatically by Xamarin Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
@@ -14,8 +14,16 @@ namespace MobileAppClass
     [Register ("QuestionsViewController")]
     partial class QuestionsViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITableView QuestionsTableView { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (QuestionsTableView != null) {
+                QuestionsTableView.Dispose ();
+                QuestionsTableView = null;
+            }
         }
     }
 }
