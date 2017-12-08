0ae6dd4471452f56e0eec0231333679f1f8a52cc
Nick <pacificonm@gmail.com>
2017-12-08 10:48:26 -0500

    Tablevieeeeeew

--- TriviaMania/TriviaMania/QuestionsViewController.cs
+++ TriviaMania/TriviaMania/QuestionsViewController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using Foundation;
 using UIKit;
 
 namespace MobileAppClass
@@ -22,5 +23,23 @@ namespace MobileAppClass
 			// Release any cached data, images, etc that aren't in use.
 		}
 	}
+
+	#region QuestionsTableView source code
+	/* public class QuestionsTableSource : UITableViewSource  //Inherit UITableViewSource library
+	{
+		QuestionsViewController vc; //An instance of QuestionsViewController
+		private List<TriviaQuestionsRecord> ListofTriviaQuestions = new List<TriviaQuestionsRecord>(); //A list of TriviaQuestionsRecords
+
+		//constructor
+		//ListofTriviaQuestions and the vc are passed into tableview
+		public QuestionsTableSource(QuestionsViewController vc_in)
+		{
+			ListofTriviaQuestions
+			vc = vc_in;
+		}
+
+	} */
+
+	#endregion
 }
 
