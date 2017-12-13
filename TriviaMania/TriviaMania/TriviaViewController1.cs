5fead0600a8e5db07e0a1880d07adc0e00f68684
Xavier <xad2@pitt.edu>
2017-12-13 01:42:09 -0500

    doesn't crash when new game is started

--- TriviaMania/TriviaMania/TriviaViewController1.cs
+++ TriviaMania/TriviaMania/TriviaViewController1.cs
@@ -175,7 +175,7 @@ namespace MobileAppClass
 		private void GameSetup()
 		{
 			// Get a random question from the list and display it
-			int rndQuestion = RandomSelectGenerator.GetInstance().RandomQuestion(ListofTriviaQuestions.Count);
+            int rndQuestion = testRandom.RandomQuestion(ListofTriviaQuestions.Count);
 
             answerBoxSelection = testRandom.AnswerBox();
 
